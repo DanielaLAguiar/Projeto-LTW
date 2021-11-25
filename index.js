3578a10f7925ef7e6ffcae31ff8e9bee4815cb20
Daniela Aguiar <up201910371@fe.up.pt>
2021-11-25 02:51:53 +0000

    Login/Logout

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const comandos=document.getElementById("comandos");
+const configuracao=document.getElementById("configuracao");
+const autenticacao=document.getElementById("autenticacao");
+const terminar=document.getElementById("terminar");
+
+function validateForm() {  
+  const name=document.getElementById("autenticacao").elements["name"].value;  
+  const password=document.getElementById("autenticacao").elements["password"].value;  
+    if (name=="Daniela" && password=="123"){
+        comandos.style.display="block";
+        configuracao.style.display="block";
+        autenticacao.style.display="none";
+        const node = document.createTextNode("Bem vindx " + name + "! " + "\u2665");
+        const logout = document.getElementById("logout");
+        terminar.insertBefore(node,logout);
+        terminar.style.display="block";
+    }
+    else {
+      alert("Login invalido!");
+    } 
+}
+
+function logout() {
+    comandos.style.display="none";
+    configuracao.style.display="none";
+    autenticacao.style.display="block";
+    terminar.style.display="none";
+    terminar.innerHTML="<input type='button' id='logout' value='Logout' onclick='logout()'>";
+    autenticacao.reset();
+  
+}
+
 function IniciarJogo() {
     const tabuleiro = document.getElementById("tabuleiro");
     const jogar = document.getElementById("jogar");
