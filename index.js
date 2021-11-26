0701582384b6f3a6fa379244f16615d4f9ff14d7
Daniela Aguiar <up201910371@fe.up.pt>
2021-11-26 00:32:36 +0000

    Configurações

--- index.js
+++ index.js
@@ -4,8 +4,47 @@ const autenticacao=document.getElementById("autenticacao");
 const terminar=document.getElementById("terminar");
 const classificacao=document.getElementById("classificacao");
 const instrucoes=document.getElementById("instrucoes");
+const nCavidades=document.getElementById("cavidades");
+const nSementes=document.getElementById("sementes");
+const iniciarJogo=document.getElementById("jogar");
+const desistir=document.getElementById("desistir");
+const tabuleiro=document.getElementById("tabuleiro");
+const estado=document.getElementById("estado");
 
-function validateForm() {  
+classificacao.onclick=this.showClassificacao.bind(this);
+instrucoes.onclick=this.showInstrucoes.bind(this);
+iniciarJogo.onclick=this.IniciarJogo.bind(this);
+desistir.onclick=this.terminarJogo.bind(this);
+
+class Jogo {
+  constructor(cavidades, sementes) {
+    this.cavidades=cavidades;
+    this.sementes=sementes;
+  }
+  
+  setTabuleiro() {
+    const arm1=document.createElement('div');
+    const arm2=document.createElement('div');
+    const feij=document.createElement('span');
+    arm1.className='cavidades';
+    arm2.className='cavidades';
+    arm1.id="armazem1";
+    arm2.id="armazem2";
+    feij.className='peca';
+    tabuleiro.appendChild(arm1);
+    for(let i=0; i<Number(this.cavidades)*2; i++) {
+      let cav=document.createElement('div');
+      cav.className='cavidades';
+      tabuleiro.appendChild(cav);
+      for(let j=0; j<Number(this.sementes);j++) {
+        cav.appendChild(feij.cloneNode());
+      }
+    }
+    tabuleiro.appendChild(arm2);
+  }
+}
+
+function login() {  
   const name=document.getElementById("autenticacao").elements["name"].value;  
   const password=document.getElementById("autenticacao").elements["password"].value;  
     if (name=="Daniela" && password=="123"){
@@ -29,11 +68,7 @@ function logout() {
     terminar.style.display="none";
     terminar.innerHTML="<input type='button' id='logout' value='Logout' onclick='logout()'>";
     autenticacao.reset();
-  
 }
-
-classificacao.onclick=this.showClassificacao.bind(this);
-instrucoes.onclick=this.showInstrucoes.bind(this);
 
 function showClassificacao() {
     const classificacaoText=document.getElementById("popClassificacao");
@@ -52,28 +87,20 @@ function showInstrucoes() {
 }
 
 function IniciarJogo() {
-    const tabuleiro = document.getElementById("tabuleiro");
-    const jogar = document.getElementById("jogar");
-    if (tabuleiro.style.display === "") {
-        tabuleiro.style.display = "flex";
-        jogar.style.background = "bisque";
-    }
-    else if (tabuleiro.style.display === "flex") {
-        tabuleiro.style.display = "";
-        jogar.style.background = "linen";
-    }
+    const jogo = new Jogo(nCavidades.valueAsNumber, nSementes.valueAsNumber);
+    jogo.setTabuleiro();
+    comandos.style.display="none";
+    configuracao.style.display="none";
+    tabuleiro.style.display="flex";
+    estado.style.display="block";
+    terminar.style.display="none";
 }
 
-function getVal() {
-    const inputCavidades = document.getElementById("cavidades");
-    let nCavidades;
-    nCavidades = inputCavidades.valueAsNumber;
-  
-    let text = "<div class=cavidades id=armazem1></div>";
-    for (let i=0; i<nCavidades*2; i++) {
-      text+="<br> <div class=cavidades> </div>";
-    }
-    text += "<div class=cavidades id=armazem1></div>";
-  
-    document.getElementById("tabuleiro").innerHTML = text;
+function terminarJogo() {
+    terminar.style.display="block";
+    comandos.style.display="block";
+    configuracao.style.display="block";
+    estado.style.display="none";
+    tabuleiro.style.display="none";
+  tabuleiro.innerHTML="";
 }
